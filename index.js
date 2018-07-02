b725fe57f3a0103292d6b0e1af78b8e94d840bef
kaline <mesquitabfkaline@gmail.com>
2018-07-01 21:34:23 -0300

    add start script

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express = require('express')
 var app = express()
 var request = require("request");
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.ELEPHANTSQL_URL || "postgres://postgres:5432@localhost/postgres";
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
+
 
 
 
