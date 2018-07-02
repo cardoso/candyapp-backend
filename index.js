6a7c198e9703431f23dad619aa9f5a9d19d9cf63
kaline <mesquitabfkaline@gmail.com>
2018-07-01 22:00:41 -0300

    fix

--- index.js
+++ index.js
@@ -5,57 +5,7 @@ var pg = require('pg');
 //or native libpq bindings
 //var pg = require('pg').native
 
-var conString = process.env.ELEPHANTSQL_URL || "postgres://postgres:5432@localhost/postgres";
-
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
-  });
-});
-
-
-
-
-
-var options = { method: 'GET',
-  url: 'http://localhost:8000',
-  headers: 
-   { 'Postman-Token': '3bc49eb4-dfc2-43ae-9a6d-4cca5c50d1e6',
-     'Cache-Control': 'no-cache' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
-
-
-
-
-var options = { method: 'POST',
-  url: 'http://localhost:8000',
-  headers: 
-   { 'Postman-Token': '95c8ce6a-8470-4f85-954f-80ce752f7d2e',
-     'Cache-Control': 'no-cache' } };
-
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  console.log(body);
-});
-
-
-
-app.listen(port = 8000, function(){
+app.listen(port = process.env.PORT || 8000, function(){
   console.info('Server listening on port ' + port);
 });
 
